78f28cd61d0676aa657b94aebc8c233ecec68088
irfanshahunity <irfan.shah@noborderz.com>
2021-11-10 10:47:50 +0500

    new changes

---
+++ Assets/Scenes/OpenFB.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFB : MonoBehaviour
+{
+    public string facebookApp;
+    public string facebookAddress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  public  void OpenFacebookPage()
+    {
+        float startTime;
+        startTime = Time.timeSinceLevelLoad;
+
+        //open the facebook app
+        Application.OpenURL(facebookApp);
+
+        if (Time.timeSinceLevelLoad - startTime <= 1f)
+        {
+            //fail. Open safari.
+            Application.OpenURL(facebookAddress);
+        }
+    }
+}
